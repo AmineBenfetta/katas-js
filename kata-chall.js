ad9bda6785c7f0be8f3452a82eb8a819f813c6d3
AmineBenfetta <amine.benfetta711@gmail.com>
2025-05-13 20:07:20 +0200

    refactor: comment out unused functions and add basicOp implementation in kata-chall.js

--- kata-chall.js
+++ kata-chall.js
@@ -68,8 +68,69 @@ console.log(findSmallestInt([2,84,-2,308]));*/
   return num;
 }*/
 
-var summation = function (num) {
-  return Math.sumPrecise(num);
+/*var summation = function (num) {
+  let sum = 0; 
+  for (let i = 1; i <= num; i++) {
+    sum += i;
+  }
+  return sum;
+}*/
+
+/*function greet(str) {
+  return "hello world";
 }
 
-console.log(summation(40));
+console.log(greet()); */
+
+/*function countSheeps(sheep) {
+  let count = 0
+  for (let i = 0; i < sheep.length; i++) {
+    if (sheep[i] === true) {
+      count++;
+    } else {
+      count;
+    }
+  }
+  return count;
+}
+
+console.log(countSheeps([true,true,true,false,true,true,true,true,true,false,true,false,true,false,false,true,true,true,true,true,false,false,true,true]))*/
+
+/*function noSpace(x){
+  return x.split(' ').join('');
+}
+
+console.log(noSpace('8 j 8   mBliB8g  imjB8B8  jl  B', '8j8mBliB8gimjB8B8jlB')); */
+
+/*function doubleInteger(i) { 
+  return i*2;
+}*/
+
+/*function booleanToString(b){
+  b = String(b);
+  return b;
+  //your code here
+}*/
+
+/*function greet(name){
+  return `Hello, ${name} how are you doing today?`; 
+  //your code here
+}*/
+
+/*function basicOp(operation, value1, value2){
+  let result;
+  if (operation === "+") {
+    result = value1 + value2; 
+  } else if (operation === "-") {
+    result = value1 - value2;
+  } else if (operation === "*") {
+    result = value1 * value2;
+  } else if (operation === "/") {
+    result = value1 / value2;
+  }
+  //Code
+  return result;
+}
+
+console.log(basicOp("-", 7, 4)); */
+
