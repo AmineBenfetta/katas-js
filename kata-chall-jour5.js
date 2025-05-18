371c4bfbc39a6ee61a257210940d611a50c79130
AmineBenfetta <amine.benfetta711@gmail.com>
2025-05-18 16:50:34 +0200

    add new function implementations for animal description, days in month, and boolean evaluation in kata-chall-jour5.js

--- kata-chall-jour5.js
+++ kata-chall-jour5.js
@@ -77,4 +77,46 @@ function popElement(arr){
 }
 */
 
-//
+// Training JS #5: Basic data types--Object
+/*function animal(obj){
+  return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
+}*/
+
+// Training JS #8: Conditional statement--switch
+/*function howManydays(month){
+  var days;
+  switch (month){
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+      days= 31;
+      break;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+      days= 30;
+      break;
+    case 2:
+      days= 28;
+      break;
+  }
+  return days;
+}*/
+
+// Is this my tail?
+
+// Training JS #6: Basic data types--Boolean and conditional statements if..else
+/*function trueOrFalse(val){ 
+  if (!val) {
+  return "false"; 
+  } else {           
+   return "true";
+}
+}*/  // "!valeur" veux dire falsy 
+
+// 
